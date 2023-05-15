73f9141c8f989a38f904ec3bcfd8e88b314a0b3c
ThatAgricBoy <samueljohn3999@gmail.com>
2023-05-15 07:38:23 -0700

    updated console

--- console.py
+++ console.py
@@ -12,7 +12,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import argparse
 
 
 class HBNBCommand(cmd.Cmd):
@@ -144,59 +143,106 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         "Usage: update <class name> <id> <attribute name> <attribute value>"
-        parser = argparse.ArgumentParser()
-        parser.add_argument('class_name', help='class name')
-        parser.add_argument('id', help='instance id')
-        parser.add_argument('attribute_name', help='attribute name')
-        parser.add_argument('attribute_value', help='attribute value')
-        args = parser.parse_args(arg.split())
+        cmd_argv = []
+        part2_argv = []
+        is_dict = 0
+        if "\"" in arg:
+            if "," in arg:
+                if "{" in arg:
+                    is_dict = 1
+                    part1_argv = arg.split(",")[0].split()
+                    for i in part1_argv:
+                        cmd_argv.append(i.replace("\"", ""))
+                    part2_argv = arg.replace("}", "").split("{")[1].split(", ")
+                    for i in part2_argv:
+                        for j in i.split(": "):
+                            cmd_argv.append(j.replace("\"", "")
+                                            .replace('\'', ""))
+                else:
+                    arg_key = arg.replace(",", "")
+                    part1_argv = arg_key.split()
+                    for i in part1_argv[:2]:
+                        cmd_argv.append(i.replace("\"", ""))
+                    part2_argv = arg.split(", ")[1:]
+                    for i in part2_argv:
+                        cmd_argv.append(i.replace("\"", ""))
+            else:
+                part1_argv = arg.split("\"")[0]
+                for i in part1_argv.split():
+                    cmd_argv.append(i)
+                part2_argv = arg.split("\"")[1:]
+                for i in part2_argv:
+                    if i != " " and i != "":
+                        cmd_argv.append(i.replace("\"", ""))
 
-        class_name = args.class_name
-        id = args.id
-        attribute_name = args.attribute_name
-        attribute_value = args.attribute_value
+        else:
+            part1_argv = arg.split()
+            for i in range(len(part1_argv)):
+                if i == 4:
+                    break
+                cmd_argv.append(part1_argv[i])
 
-        if not class_name:
+        if (len(cmd_argv) == 0):
             print("** class name missing **")
             return None
 
         try:
-            cls = eval(class_name)
-        except NameError:
+            eval(cmd_argv[0])
+        except Exception:
             print("** class doesn't exist **")
             return None
 
-        all_objs = storage.all(cls)
-
-        key = "{}.{}".format(class_name, id)
-        if key not in all_objs:
-            print("** no instance found **")
+        if len(cmd_argv) < 2:
+            print("** instance id missing **")
             return None
 
-        obj = all_objs[key]
-        setattr(obj, attribute_name, attribute_value)
-        obj.save()
+        all_objs = storage.all()
+
+        key = cmd_argv[0] + '.' + cmd_argv[1]
+        if all_objs.get(key, False):
+            if (len(cmd_argv) >= 3):
+                if (len(cmd_argv) % 2) == 0:
+                    for i in range(2, len(cmd_argv), 2):
+                        attr = cmd_argv[i]
+                        type_att = getattr(all_objs[key], cmd_argv[i], "")
+                        try:
+                            cast_val = type(type_att)(cmd_argv[i + 1])
+                        except Exception:
+                            cast_val = type_att
+                        setattr(all_objs[key], cmd_argv[i], cast_val)
+                        all_objs[key].save()
+                        if is_dict == 0:
+                            break
+                else:
+                    print("** value missing **")
+            else:
+                print("** attribute name missing **")
+        else:
+            print("** no instance found **")
 
     def do_count(self, arg):
         "Usage: count <class name> or <class name>.count()"
-        parser = argparse.ArgumentParser()
-        parser.add_argument('class_name', help='class name', nargs='?')
-        args = parser.parse_args(arg.split())
+        cmd_argv = arg.split()
 
-        class_name = args.class_name
-
-        if not class_name:
-            count = len(storage.all())
-        else:
+        if cmd_argv:
             try:
-                cls = eval(class_name)
-            except NameError:
+                eval(cmd_argv[0])
+            except Exception:
                 print("** class doesn't exist **")
                 return None
 
-            count = len(storage.all(cls))
+        all_objs = storage.all()
+        count = 0
 
+        for key, value in all_objs.items():
+            if not cmd_argv:
+                count += 1
+            else:
+                check = key.split('.')
+                if cmd_argv[0] == check[0]:
+                    count += 1
         print(count)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
